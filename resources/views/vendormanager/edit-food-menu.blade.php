4d3920371d53ef107def055e5e4d4b66568c23d4
Esther Eze <bettatechsolutions@gmail.com>
2024-07-15 13:58:59 +0100

    Update edit-food-menu.blade.php

--- resources/views/vendormanager/edit-food-menu.blade.php
+++ resources/views/vendormanager/edit-food-menu.blade.php
@@ -8,7 +8,7 @@
       <div class="content-wrapper">
             <div class="page-header">
                   <h3 class="page-title">
-                        Edit >>> Food Menu for {{$menu->vendor_name}}
+                        Edit >>> Food Menu 
                   </h3>
             </div>
             <!--Alert here--->
