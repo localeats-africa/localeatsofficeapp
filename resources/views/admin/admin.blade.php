df54f2468f0b83ce6f1498f07a754a3210b2975d
Esther Eze <bettatechsolutions@gmail.com>
2024-07-17 06:50:26 +0100

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -88,6 +88,72 @@
                         </div>
                   </div>
                   <!--row-deck-->
+
+                  <div class="row ">
+                        <div class="col-12">
+                              <div class="row row-cards">
+                                    <div class="col-md-3 stretch-card grid-margin">
+                                          <div class="card bg-gradient-danger card-img-holder text-white">
+                                                <div class="card-body">
+                                                      <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
+                                                            class="card-img-absolute" alt="circle-image">
+                                                      <h4 class="font-weight-normal">Number Of Orders <i
+                                                                  class="mdi mdi-pot-steam  mdi-24px float-end"></i>
+                                                      </h4>
+                                                      <h2 class="mb-5">{{$countAllOrder}}</h2>
+                                                      <hr class="w-100">
+                                                      <h6 class="card-text">From  <span class="text-dark">( {{$countPlatformWhereOrderCame}} )</span>
+                                                            <span
+                                                                  style="float:right;">platform (s)</span> 
+
+                                                      </h6>
+                                                </div>
+
+                                          </div>
+                                    </div>
+
+                                    <div class="col-md-3 stretch-card grid-margin">
+                                          <div class="card bg-gradient-success card-img-holder text-white">
+                                                <div class="card-body">
+                                                      <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
+                                                            class="card-img-absolute" alt="circle-image">
+                                                      <h4 class="font-weight-normal">Number Of Plates <i
+                                                                  class="mdi mdi-pot-steam  mdi-24px float-end"></i>
+                                                      </h4>
+                                                      <h2 class="mb-5">{{$countAllPlate}}</h2>
+                                                      <hr class="w-100">
+                                                      <h6 class="card-text">From  <span class="text-dark">( {{$countAllOrder}} )</span>
+                                                            <span
+                                                                  style="float:right;">order (s)</span> 
+
+                                                      </h6>
+                                                </div>
+
+                                          </div>
+                                    </div>
+
+                                    <div class="col-md-3 stretch-card grid-margin">
+                                          <div class="card bg-gradient-info card-img-holder text-white">
+                                                <div class="card-body">
+                                                      <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
+                                                            class="card-img-absolute" alt="circle-image">
+                                                      <h4 class="font-weight-normal">Orders <i
+                                                                  class="mdi mdi-shopping mdi-24px float-end"></i>
+                                                      </h4>
+                                                      <h2 class="mb-5">₦{{number_format($sumAllOrders)}}</h2>
+                                                      <hr class="w-100">
+                                                      <h6 class="card-text">weekly average <span style="float:right;">₦0
+                                                            </span></h6>
+                                                </div>
+                                          </div>
+                                    </div>
+
+         
+                              </div>
+                              <!--row--->
+                        </div>
+                  </div>
+                  <!--row-deck-->
             </div>
             <p></p>
             <!--Alert here--->
