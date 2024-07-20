12e4b79d38cb3f4cdd788081eef18e9f19ef83af
Esther Eze <bettatechsolutions@gmail.com>
2024-07-20 14:42:06 +0100

    Update all-staff.blade.php

--- resources/views/admin/all-staff.blade.php
+++ resources/views/admin/all-staff.blade.php
@@ -43,6 +43,26 @@
             <!--Alert here--->
             <div class="row ">
                   <div class="col-12">
+                  @if(session('staff-assign'))
+                        <div class="alert  alert-success alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                          </svg>
+                                    </div>
+                                    <div> {!! session('staff-assign') !!}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
 
                         @if(session('staff-status'))
                         <div class="alert  alert-danger alert-dismissible" role="alert">
