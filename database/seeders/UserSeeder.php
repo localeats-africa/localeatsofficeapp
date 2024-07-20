15a84a772d2e40db82ed76fa725912e26e9fb65b
Esther Eze <bettatechsolutions@gmail.com>
2024-07-20 11:32:05 +0100

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,11 +20,27 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
-      
+        DB::table('users')->insert([
+            'role_id'   => '1',
+            'fullname'      => 'SuperAdmin',
+            'email'     => 'admin@localeats.africa',
+            'password'  => Hash::make('12345678'),
+            'email_verified_at' => Carbon::now()
+            
+        ]);
+
+        DB::table('users')->insert([
+            'role_id'   => '2',
+            'fullname'      => 'Admin',
+            'email'     => 'estherakowe13@gmail.com',
+            'password'  => Hash::make('12345678'),
+            'email_verified_at' => Carbon::now()
+            
+        ]);
 
         DB::table('users')->insert([
             'role_id'   => '6',
-            'name'      => 'Vendor Account',
+            'fullname'      => 'Vendor Account',
             'email'     => 'techvendor@localeats.africa',
             'password'  => Hash::make('12345678'),
             'email_verified_at' => Carbon::now()
