4acee189142bcf45290aa07f9b0eb5e905543a2c
Esther Eze <bettatechsolutions@gmail.com>
2024-07-20 13:50:44 +0100

    Update all-staff.blade.php

--- resources/views/admin/all-staff.blade.php
+++ resources/views/admin/all-staff.blade.php
@@ -10,13 +10,20 @@
                   <h3 class="page-title">
                         All Staff (s)
                   </h3>
+                  <nav aria-label="breadcrumb">
+                <ul class="breadcrumb">
+                  <li class="breadcrumb-item active" aria-current="page">
+                    <span></span><a href="{{ url('new-staff') }}" class="btn btn-block btn-danger"><i class="fa fa-plus-square"></i> &nbsp;New Staff </a> 
+                  </li>
+                </ul>
+              </nav>
             </div>
             <p></p>
             <div class="row ">
                   <div class="col-12">
                         <div class="row row-cards">
                               <div class="col-md-4 stretch-card grid-margin">
-                                    <div class="card bg-gradient-danger card-img-holder text-white">
+                                    <div class="card bg-gradient-info card-img-holder text-white">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
                                                       class="card-img-absolute" alt="circle-image">
