8089c15bb950919093b2a55770ed21aab8c3c48c
Esther Eze <bettatechsolutions@gmail.com>
2024-07-22 18:58:04 +0100

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -178,7 +178,7 @@
                                                       </h4>
                                                       <h2 class="mb-5">₦ {{number_format($commission, 2) }}</h2>
                                                       <hr class="w-100">
-                                                      <h6 class="card-text">weekly average<span style="float:right;">₦0
+                                                      <h6 class="card-text">weekly average<span style="float:right;">₦{{number_format($averageComm, 2)}}
                                                             </span> </h6>
                                                 </div>
                                           </div>
