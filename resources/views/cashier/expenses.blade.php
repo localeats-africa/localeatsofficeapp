fbd34c7bc61a412af48ae5f6178cf5a7c1896dff
Esther Eze <bettatechsolutions@gmail.com>
2024-07-22 17:45:09 +0100

    Update expenses.blade.php

--- resources/views/cashier/expenses.blade.php
+++ resources/views/cashier/expenses.blade.php
@@ -121,6 +121,180 @@
                   </form>
             </div>
             <!---end row --->
+
+            <p></p>
+            <div class="row ">
+                  <div class="col-12">
+                        <div class="card">
+                              <div class="card-header">
+                                    <h4 class="card-title"> </h4>
+                              </div>
+                              <div class="card-body border-bottom py-3">
+                                    <div class="d-flex">
+                                          <div class="text-secondary">
+                                                Show
+                                                <div class="mx-2 d-inline-block">
+                                                      <select id="pagination" class="form-control form-control-sm"
+                                                            name="perPage">
+                                                            <option value="5" @if($perPage==5) selected @endif>5
+                                                            </option>
+                                                            <option value="10" @if($perPage==10) selected @endif>
+                                                                  10
+                                                            </option>
+                                                            <option value="25" @if($perPage==25) selected @endif>
+                                                                  25
+                                                            </option>
+                                                            <option value="50" @if($perPage==50) selected @endif>
+                                                                  50
+                                                            </option>
+                                                      </select>
+                                                </div>
+                                                records
+                                          </div>
+                                          <div class="ms-auto text-secondary">
+                                                Search:
+                                                <div class="ms-2 d-inline-block">
+
+                                                      <form action="{{ route('all-platform') }}" method="GET" role="search">
+                                                            {{ csrf_field() }}
+                                                            <div class="input-group mb-2">
+                                                                  <input type="text" class="form-control"
+                                                                        placeholder="Search for…" name="search">
+                                                                  <button type="submit" class="btn"
+                                                                        type="button">Go!</button>
+                                                            </div>
+                                                      </form>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
+
+                              <div class="table-responsive " id="card">
+                                    <table class="table table-striped card-table table-vcenter text-nowrap datatable"
+                                          id="orders">
+                                          <thead>
+                                                <tr>
+                                                      <th class="w-1"><input class="form-check-input m-0 align-middle"
+                                                                  type="checkbox" aria-label="Select all product">
+                                                      </th>
+                                                      <th>Logo</th>
+                                                      <th>Platform</th>
+                                                      <th>Vendors</th>
+                                                      <th>Order (s)</th>
+                                                      <th></th>
+                                                </tr>
+                                          </thead>
+                                          <tbody>
+                                                @foreach($platform as $data)
+                                                <tr>
+                                                      <td><input class="form-check-input m-0 align-middle"
+                                                                  type="checkbox" aria-label="Select"></td>
+                                                      <td class="py-1">
+                                                            @if(empty($data->img_url))
+                                                            None
+                                                            @else
+                                                            <img src="{{ asset($data->img_url) }}" class="cursor"
+                                                                  style="">
+                                                            @endif
+                                                      </td>
+
+                                                      <td class="text-capitalize">{{$data->name}}</td>
+
+                                                      <td>
+                                                            @if($data->name == 'Chowdeck')
+                                                            {{$activeChowdeckVendor->count()}}
+                                                            @endif
+
+                                                            @if($data->name == 'Glovo')
+                                                            {{$activeGlovoVendor->count()}}
+                                                            @endif
+
+                                                            @if($data->name == 'Edenlife')
+                                                            {{$activeEdenlifeVendor->count()}}
+                                                            @endif
+                                                      </td>
+                                                     
+                                                      <td>0</td>
+
+                                                      <td class="text-end">
+                                                            <span class="dropdown">
+                                                                  <button
+                                                                        class="btn dropdown-toggle align-text-top text-danger"
+                                                                        data-bs-boundary="viewport"
+                                                                        data-bs-toggle="dropdown">Actions</button>
+                                                                  <div class="dropdown-menu ">
+                                                                        <a class="dropdown-item text-capitalize text-dark"
+                                                                              href="delete-platform/{{$data->id}}">
+                                                                              <small>Delete</small>
+                                                                        </a>
+
+                                                                  </div>
+                                                            </span>
+                                                      </td>
+
+                                                </tr>
+                                                @endforeach
+
+                                          </tbody>
+
+                                    </table>
+                              </div>
+                              <div class="card-footer d-flex align-items-center">
+                                    <p class="m-0 text-secondary">
+
+                                          Showing
+                                          {{ ($platform->currentPage() - 1) * $platform->perPage() + 1; }} to
+                                          {{ min($platform->currentPage()* $platform->perPage(), $platform->total()) }}
+                                          of
+                                          {{$platform->total()}} entries
+                                    </p>
+
+                                    <ul class="pagination m-0 ms-auto">
+                                          @if(isset($platform))
+                                          @if($platform->currentPage() > 1)
+                                          <li class="page-item ">
+                                                <a class="page-link text-danger"
+                                                      href="{{ $platform->previousPageUrl() }}" tabindex="-1"
+                                                      aria-disabled="true">
+                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
+                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M15 6l-6 6l6 6" />
+                                                      </svg>
+                                                      prev
+                                                </a>
+                                          </li>
+                                          @endif
+
+
+                                          <li class="page-item">
+                                                {{ $platform->appends(compact('perPage'))->links()  }}
+                                          </li>
+                                          @if($platform->hasMorePages())
+                                          <li class="page-item">
+                                                <a class="page-link text-danger" href="{{ $platform->nextPageUrl() }}">
+                                                      next
+                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
+                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M9 6l6 6l-6 6" />
+                                                      </svg>
+                                                </a>
+                                          </li>
+                                          @endif
+                                          @endif
+                                    </ul>
+                              </div>
+                        </div>
+                        <!--- card-->
+                  </div>
+            </div>
       </div>
       <!--- content wrapper---->
       <!-- partial -->
