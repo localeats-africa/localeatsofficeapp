0fe11b5862eb9308ffa39d8db59c9ed1a82e3762
Esther Eze <bettatechsolutions@gmail.com>
2024-07-22 14:10:42 +0100

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -57,7 +57,7 @@
                                                 <div class="card-body">
                                                       <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
                                                             class="card-img-absolute" alt="circle-image">
-                                                      <h4 class="font-weight-normal">Number Of Sales <i
+                                                      <h4 class="font-weight-normal">Number Of Orders <i
                                                                   class="mdi mdi-shopping  mdi-24px float-end"></i>
                                                       </h4>
                                                       <h2 class="mb-5">{{$countAllOrder}}</h2>
@@ -105,7 +105,7 @@
                               <div class="row row-cards">
 
 
-                                    <div class="col-md-4 stretch-card grid-margin">
+                                    <div class="col-md-3 stretch-card grid-margin">
                                           <div class="card bg-gradient-info card-img-holder text-white">
                                                 <div class="card-body">
                                                       <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
@@ -141,8 +141,8 @@
                                           </div>
                                     </div>
 
-                                    <div class="col-sm-4  col-12 stretch-card grid-margin">
-                                          <div class="card bg-success card-img-holder text-white">
+                                    <div class="col-sm-3 col-12 stretch-card grid-margin">
+                                          <div class="card bg-gradient-primary card-img-holder text-white">
                                                 <div class="card-body">
                                                       <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
                                                             class="card-img-absolute" alt="circle-image">
@@ -157,12 +157,29 @@
                                           </div>
                                     </div>
 
-                                    <div class="col-sm-4  col-12 stretch-card grid-margin">
+                                    <div class="col-sm-3  col-12 stretch-card grid-margin">
                                           <div class="card bg-primary card-img-holder text-white">
                                                 <div class="card-body">
                                                       <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
                                                             class="card-img-absolute" alt="circle-image">
-                                                      <h4 class="font-weight-normal"> Commission
+                                                      <h4 class="font-weight-normal"> Expected Commission
+                                                            <i class="mdi mdi-hand-coin mdi-24px float-end"></i>
+                                                      </h4>
+                                                      <h2 class="mb-5">₦ {{number_format($commission) }}</h2>
+                                                      <hr class="w-100">
+                                                      <h6 class="card-text">weekly average<span style="float:right;">₦0
+                                                            </span> </h6>
+                                                </div>
+                                          </div>
+                                    </div>
+
+
+                                    <div class="col-sm-3  col-12 stretch-card grid-margin">
+                                          <div class="card bg-success card-img-holder text-white">
+                                                <div class="card-body">
+                                                      <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
+                                                            class="card-img-absolute" alt="circle-image">
+                                                      <h4 class="font-weight-normal">  Commission Paid
                                                             <i class="mdi mdi-hand-coin mdi-24px float-end"></i>
                                                       </h4>
                                                       <h2 class="mb-5">₦ {{number_format($commission) }}</h2>
