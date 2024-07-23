17eac6092433666cbd1a43b0530d12758a10ba63
Esther Eze <bettatechsolutions@gmail.com>
2024-07-23 02:44:03 +0100

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -146,7 +146,7 @@
                                                       </h4>
                                                       <h2 class="mb-5">₦{{number_format($sumAllOrders, 2)}}</h2>
                                                       <hr class="w-100">
-                                                      <h6 class="card-text">weekly average <span style="float:right;">₦{{number_format($averageWeeklySales, 2)}}
+                                                      <h6 class="card-text">last 7 days <span style="float:right;">₦{{number_format($averageWeeklySales, 2)}}
                                                             </span></h6>
                                                 </div>
                                           </div>
@@ -162,7 +162,7 @@
                                                       </h4>
                                                       <h2 class="mb-5">₦{{number_format($payouts, 2)}}</h2>
                                                       <hr class="w-100">
-                                                      <h6 class="card-text">weekly average <span style="float:right;">₦{{number_format($averageWeeklyPayouts, 2)}}
+                                                      <h6 class="card-text">last 7 days <span style="float:right;">₦{{number_format($averageWeeklyPayouts, 2)}}
                                                             </span> </h6>
                                                 </div>
                                           </div>
@@ -178,7 +178,7 @@
                                                       </h4>
                                                       <h2 class="mb-5">₦ {{number_format($commission, 2) }}</h2>
                                                       <hr class="w-100">
-                                                      <h6 class="card-text">weekly average<span style="float:right;">₦{{number_format($averageWeeklyComm, 2)}}
+                                                      <h6 class="card-text">last 7 days<span style="float:right;">₦{{number_format($averageWeeklyComm, 2)}}
                                                             </span> </h6>
                                                 </div>
                                           </div>
@@ -195,7 +195,7 @@
                                                       </h4>
                                                       <h2 class="mb-5">₦ {{number_format($commissionPaid, 2) }}</h2>
                                                       <hr class="w-100">
-                                                      <h6 class="card-text">weekly average<span style="float:right;">₦{{number_format($averageWeeklyCommissionPaid,2)}}
+                                                      <h6 class="card-text">last 7 days<span style="float:right;">₦{{number_format($averageWeeklyCommissionPaid,2)}}
                                                             </span> </h6>
                                                 </div>
                                           </div>
