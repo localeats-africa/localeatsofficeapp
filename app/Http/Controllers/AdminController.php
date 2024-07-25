e55cfb498007ea0282caf7de1b15c21269f7c4b2
Esther Eze <bettatechsolutions@gmail.com>
2024-07-25 14:08:28 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1053,8 +1053,44 @@ class AdminController extends Controller
        }
        else{
          return redirect()->back()->with('expense-error', 'Opps!');
-       }
-  
+       } 
+     }
+
+     public function showDeletedInvoice(Request $request){
+        $name = Auth::user()->name;
+        $user_id = Auth::user()->id;
+        $role = DB::table('role')->select('role_name')
+        ->join('users', 'users.role_id', 'role.id')
+        ->where('users.id', $user_id)
+        ->pluck('role_name')->first();
+
+        $perPage = $request->perPage ?? 25;
+        $search = $request->input('search');
+
+
+        $orders = DB::table('orders')->distinct()
+        //->join('merge_invoices', 'orders.number_of_order_merge', '=', 'merge_invoices.number_of_order_merge')
+        ->join('vendor', 'orders.vendor_id', '=', 'vendor.id')
+        ->where('orders.deleted_at', '!=', null)
+        ->orderBy('orders.created_at', 'desc')
+        ->select(['orders.*', 
+        'vendor.vendor_name', 'vendor.id' ])
+        ->where(function ($query) use ($search) {  // <<<
+        $query->where('orders.created_at', 'LIKE', '%'.$search.'%')
+               ->orWhere('vendor.vendor_name', 'LIKE', '%'.$search.'%')
+               ->orWhere('orders.invoice_ref', 'LIKE', '%'.$search.'%')
+               ->orderBy('orders.created_at', 'desc');
+        })->paginate($perPage, $columns = ['*'], $pageName = 'orders'
+        )->appends(['per_page'   => $perPage]);
+
+     
+        $pagination = $orders->appends ( array ('search' => $search) );
+            if (count ( $pagination ) > 0){
+                return view('admin.restore-invoices',  compact(
+                'perPage', 'name', 'role', 'orders'))->withDetails( $pagination );     
+            } 
+            else{return redirect()->back()->with('invoice-status', 'No record order found');}
+        return view('admin.restore-invoices', compact('name', 'role', 'orders'));
      }
 
 }//class
