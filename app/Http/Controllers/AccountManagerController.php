46346647225f6e6a05f9db7a895eadb433075174
Esther Eze <bettatechsolutions@gmail.com>
2024-07-30 11:01:44 +0100

    Create AccountManagerController.php

---
+++ app/Http/Controllers/AccountManagerController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use App\Models\SalesPlatform;
+use App\Models\BankList;
+use App\Models\FoodType;
+use App\Models\FoodMenu;
+use App\Models\RestaurantType;
+use App\Models\Vendor;
+use App\Models\Area;
+use Excel;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Auth;
+use Validator;
+use Session;
+use Paystack;
+use Storage;
+use Mail;
+use Notification;
+use DateTime;
+
+class AccountManagerController extends Controller
+{
+    public function __construct(){
+        $this->middleware(['auth', 'user-access:8', 'verified']);
+    }
+}
