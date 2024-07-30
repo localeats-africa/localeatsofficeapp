46bd3c31211eb560a8ad54a979536d424c69a1e8
Esther Eze <bettatechsolutions@gmail.com>
2024-07-30 11:14:58 +0100

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -291,7 +291,6 @@
                               </ul>
                         </div>
                   </li>
-
                   @endif
 
                   <!--- Cashier sidebar ---->
@@ -326,6 +325,103 @@
                         </div>
                   </li>
                   @endif
+
+                  <!-- -Account Manager -->
+                  @if(Auth::user()->role_id == '8')
+                  <li class="nav-item">
+                        <a class="nav-link" href="{{ url('accountmanager')}}">
+                              <span class="menu-title">Dashboard</span>
+                              <i class="mdi mdi-home menu-icon"></i>
+                        </a>
+                  </li>
+
+                  <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#vendor" aria-expanded="false"
+                              aria-controls="vendor">
+                              <span class="menu-title">Vendor</span>
+                              <i class="menu-arrow"></i>
+                              <i class="mdi mdi-pot-steam   menu-icon fs-24"></i>
+                        </a>
+                        <div class="collapse" id="vendor">
+                              <ul class="nav flex-column sub-menu">
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="{{ url('new-vendor')}}">Add New Vendor</a>
+                                    </li>
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="{{ url('all-vendor')}}">All Vendors</a>
+                                    </li>
+
+                              </ul>
+                        </div>
+                  </li>
+
+
+                  <li class="nav-item">
+                        <a class="nav-link" href="{{ url('setup-vendor') }}" target="">
+                              <span class="menu-title">Setup</span>
+                              <i class="mdi mdi-cog-outline fs-24 menu-icon"></i>
+
+                        </a>
+                  </li>
+
+                  <li class="nav-item">
+                        <a class="nav-link" href="{{ url('all-food-menu') }}" >
+                              <span class="menu-title">Food Menu</span>
+                              <i class="mdi mdi-menu   menu-icon fs-24"></i>
+
+                        </a>
+                  </li>
+
+                  <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#invoice" aria-expanded="false"
+                              aria-controls="invoice">
+                              <span class="menu-title">Invoice (s)</span>
+                              <i class="menu-arrow"></i>
+                              <i class="mdi mdi-invoice-text-multiple   menu-icon fs-24"></i>
+
+                        </a>
+                        <div class="collapse" id="invoice">
+                              <ul class="nav flex-column sub-menu">
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="{{ url('create-invoice') }}"> New Invoice </a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="{{ url('vendor-merged-invoices') }}">Merged
+                                                Invoices</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="{{ url('all-invoices') }}">Final Invoices</a>
+                                    </li>
+
+                              </ul>
+                        </div>
+                  </li>
+                  <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#food-menu" aria-expanded="false"
+                              aria-controls="food-menu">
+                              <span class="menu-title">Bookkeeping</span>
+                              <i class="menu-arrow"></i>
+                              <i class="mdi mdi-menu   menu-icon fs-24"></i>
+
+                        </a>
+                        <div class="collapse" id="food-menu">
+                              <ul class="nav flex-column sub-menu">
+
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="{{ url('add-expenses') }}">Expenses</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="{{ url('offline-sales') }}">Sales</a>
+                                    </li>
+
+                              </ul>
+                        </div>
+                  </li>
+                  @endif
+                  <!---end account manager --->
                   @endauth
             </ul>
 
