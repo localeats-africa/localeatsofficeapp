7cce4b32bb9378594b710f15e07d0fc05d393d4d
Esther Eze <bettatechsolutions@gmail.com>
2024-07-30 11:10:33 +0100

    Update AccountManagerController.php

--- app/Http/Controllers/AccountManagerController.php
+++ app/Http/Controllers/AccountManagerController.php
@@ -85,4 +85,49 @@ class AccountManagerController extends Controller
        }
     }
 
+    public function allVendors(Request $request){
+     
+        $name = Auth::user()->name;
+        $id = Auth::user()->id; 
+        $role = DB::table('role')->select('role_name')
+        ->join('users', 'users.role_id', 'role.id')
+        ->where('users.id', $id)
+        ->pluck('role_name')->first();
+
+        $perPage = $request->perPage ?? 10;
+        $search = $request->input('search');
+        $vendor = DB::table('vendor')
+        ->join('restaurant_type', 'restaurant_type.id', 'vendor.restaurant_type')
+        ->join('food_type', 'food_type.id', 'vendor.food_type')
+        ->join('state', 'state.id', 'vendor.state_id')
+        ->join('country', 'country.id', 'vendor.country_id')
+        ->select(['vendor.*', 
+        'restaurant_type.restaurant_type', 
+        'food_type.food_type', 
+        'state.state', 
+        'country.country'])
+        ->orderBy('vendor.created_at', 'desc')
+        ->where(function ($query) use ($search) {  // <<<
+        $query->where('vendor.vendor_ref', 'LIKE', '%'.$search.'%')
+        ->orWhere('vendor.vendor_name', 'LIKE', '%'.$search.'%')
+        ->orWhere('vendor.food_type', 'LIKE', '%'.$search.'%')
+        ->orWhere('vendor.contact_phone', 'LIKE', '%'.$search.'%')
+        ->orWhere('vendor.email', 'LIKE', '%'.$search.'%')
+        ->orWhere('vendor.state', 'LIKE', '%'.$search.'%')
+        ->orWhere('vendor.country', 'LIKE', '%'.$search.'%')
+        ->orWhere('vendor.status', 'LIKE', '%'.$search.'%')
+        ->orderBy('vendor.created_at', 'desc');
+        })->paginate($perPage, $columns = ['*'], $pageName = 'vendor'
+        )->appends(['per_page'   => $perPage]);
+    
+        $pagination = $vendor->appends ( array ('search' => $search) );
+        if (count ( $pagination ) > 0){
+            return view('vendormanager.manager',  compact(
+            'perPage', 'name', 'role', 'vendor'))->withDetails( $pagination );     
+        } 
+        else{return redirect()->back()->with('vendor-status', 'No record order found'); }
+        return view('vendormanager.manager',  compact('perPage' , 'name', 'role', 
+        'vendor'));
 }
+
+}//class
