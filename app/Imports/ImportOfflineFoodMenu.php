9c4729c7f96c84a65a70f37b15464aeac25a1f4f
Esther Eze <bettatechsolutions@gmail.com>
2024-08-02 12:03:24 +0100

    Update ImportOfflineFoodMenu.php

--- app/Imports/ImportOfflineFoodMenu.php
+++ app/Imports/ImportOfflineFoodMenu.php
@@ -16,10 +16,21 @@ class ImportOfflineFoodMenu implements ToModel
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+
+    public function  __construct(string $vendor_id) {
+        $this->vendor_id= $vendor_id;
+    }
+
     public function model(array $row)
     {
         return new OfflineFoodMenu([
             //
+            'swallow'      => $row[0],
+            'soup'         => $row[1],
+            'protein'      => $row[2],
+            'others'       => $row[3],
+            'added_by'     => Auth::id(),
+            'vendor_id'    => $this->vendor_id,
         ]);
     }
 }
