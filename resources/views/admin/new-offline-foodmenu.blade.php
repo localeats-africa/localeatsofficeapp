32a21863fce7eda4636d15c3dc4cba12c201fc88
Esther Eze <bettatechsolutions@gmail.com>
2024-08-02 23:10:18 +0100

    Update new-offline-foodmenu.blade.php

--- resources/views/admin/new-offline-foodmenu.blade.php
+++ resources/views/admin/new-offline-foodmenu.blade.php
@@ -236,7 +236,7 @@
                                                       <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
                                                       <path d="M14 4l0 4l-6 0l0 -4" />
                                                 </svg>
-                                                Add Expenses List
+                                                Add Food Menu
                                           </button>
                                     </div>
                               </div>
