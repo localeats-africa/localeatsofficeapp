c3069131925bc7cc93d212321ef7f88cd3e28f26
Esther Eze <estherakowe@yahoo.com>
2024-09-13 17:11:51 +0100

    Update 2024_09_12_140015_create_multi_store_role_table.php

--- database/migrations/2024_09_12_140015_create_multi_store_role_table.php
+++ database/migrations/2024_09_12_140015_create_multi_store_role_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('multi_store_role', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('multi_store_id');
             $table->string('role');
             $table->string('level');
         });
