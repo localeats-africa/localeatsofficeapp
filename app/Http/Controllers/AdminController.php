0575af80cb8af237ff6bf3a93faffcdeec8d909a
Esther Eze <estherakowe@yahoo.com>
2024-09-14 14:54:02 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1862,7 +1862,7 @@ class AdminController extends Controller
 
         $state = State::all();
 
-        $perPage = $request->perPage ?? 25;
+        $perPage = $request->perPage ?? 20;
         $search = $request->input('search');
 
         $location = DB::table('area')
