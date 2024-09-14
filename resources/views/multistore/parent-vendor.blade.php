4361b970e887ce84d6633c3329967f7dfbfc9225
Esther Eze <estherakowe@yahoo.com>
2024-09-14 17:22:58 +0100

    Update parent-vendor.blade.php

--- resources/views/multistore/parent-vendor.blade.php
+++ resources/views/multistore/parent-vendor.blade.php
@@ -168,7 +168,7 @@
 
                   @foreach($vendor as $data)
                   @php
-                  $words = explode(" ", $data->vendor_name, 2 );
+                  $words = explode(" ", $data->store_name, 2 );
                   $initials = null;
                   foreach ($words as $w) {
                   $initials .= $w[0];
@@ -285,7 +285,7 @@
                                     </div>
                                     <span class="avatar avatar-xl mb-3 rounded">{{$initials}}</span>
                                     <h6 class="m-0 mb-1">
-                                          {!! Str::limit("$data->vendor_name", 25, '...') !!}
+                                          {!! Str::limit("$data->store_name", 25, '...') !!}
                                     </h6>
                                     <div class="text-dark"> <small>{{$data->store_area}}, {{$data->state}}</small></div>
 
