34f73fa95d2464f1e484e5d2a17fd3c5e74d18af
Esther Eze <estherakowe@yahoo.com>
2024-09-14 19:33:44 +0100

    Create ParentVendorController.php

---
+++ app/Http/Controllers/ParentVendorController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\SalesPlatform;
+use App\Models\Platforms;
+use App\Models\BankList;
+use App\Models\FoodType;
+use App\Models\FoodMenu;
+use App\Models\RestaurantType;
+use App\Models\Vendor;
+use App\Models\Orders;
+use App\Models\Area;
+use App\Models\State;
+use App\Models\Role;
+use App\Models\Level;
+use App\Models\Commission;
+use App\Mail\NewUserEmail;
+use App\Models\ExpensesList;
+use App\Models\OfflineSales;
+use App\Models\VendorExpenses;
+use App\Models\OfflineFoodMenu;
+use App\Models\MultiStoreRole;
+use App\Models\MultiStore;
+use App\Models\SubStore;
+
+use Excel;
+use Auth;
+use Session;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Mail;
+
+
+class ParentVendorController extends Controller
+{
+    public function __construct(){
+        $this->middleware(['auth', 'user-access:9', 'verified']);
+    }
+}
