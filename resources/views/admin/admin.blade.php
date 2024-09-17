6ef47627bb2c36d77c4247235f2f364d9ab29e9f
Esther Eze <estherakowe@yahoo.com>
2024-09-17 11:33:34 +0100

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -359,7 +359,7 @@
                         <div class="card">
                               <div class="card-body">
                                     <h4 class="card-title">Sales chart:</h4>
-                                    <canvas id="lineChart" style="height: 55px !important; width: 125px;"></canvas>
+                                    <canvas id="areaChart" style="height:250px"></canvas>
                               </div>
                         </div>
                   </div>
@@ -805,5 +805,53 @@ var myChart = new Chart(ctx, {
             }
       }
 });
+</script>
+
+<script>
+      var areaData = {
+      labels: @json($data['month']),
+      datasets: [{
+      label: @json($salesYear),
+      data: @json($data['sales']),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+      fill: true, // 3: no fill
+    }]
+  };
+
+  var areaOptions = {
+    elements: {
+      line: {
+        tension: 0.5
+      }
+    },
+    plugins: {
+      filler: {
+        propagate: true
+      }
+    }
+  }
+    var areaChartCanvas =document.getElementById('areaChart').getContext('2d');
+    var areaChart = new Chart(areaChartCanvas, {
+      type: 'line',
+      data: areaData,
+      options: areaOptions
+    });
+
 </script>
 @endsection
