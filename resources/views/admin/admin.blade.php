c8581c2cdd3cc763e4157645550c9aeb838e5e05
Esther Eze <estherakowe@yahoo.com>
2024-09-17 10:47:58 +0100

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -168,7 +168,7 @@
             <div class="row ">
                   <div class="col-12">
                         <div class="row row-cards">
-                              <div class="col-md-3 stretch-card grid-margin">
+                              <div class="col-md-4 stretch-card grid-margin">
                                     <div class="card bg-gradient-info card-img-holder text-white">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
@@ -204,7 +204,7 @@
                                     </div>
                               </div>
 
-                              <div class="col-sm-3 col-12 stretch-card grid-margin">
+                              <div class="col-sm-4 col-12 stretch-card grid-margin">
                                     <div class="card bg-gradient-primary card-img-holder text-white">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
@@ -221,7 +221,7 @@
                                     </div>
                               </div>
 
-                              <div class="col-sm-3  col-12 stretch-card grid-margin">
+                              <div class="col-sm-4  col-12 stretch-card grid-margin">
                                     <div class="card bg-primary card-img-holder text-white">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
@@ -239,7 +239,13 @@
                               </div>
 
 
-                              <div class="col-sm-3  col-12 stretch-card grid-margin">
+                            
+                        </div>
+                        <!--row--->
+
+                        <!--row-->
+                        <div class="row row-cards">
+                        <div class="col-sm-4  col-12 stretch-card grid-margin">
                                     <div class="card bg-success card-img-holder text-white">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
@@ -255,12 +261,8 @@
                                           </div>
                                     </div>
                               </div>
-                        </div>
-                        <!--row--->
 
-                        <!--row-->
-                        <div class="row row-cards">
-                              <div class="col-sm-3 col-12 stretch-card grid-margin">
+                              <div class="col-sm-4 col-12 stretch-card grid-margin">
                                     <div class="card bg-danger card-img-holder text-white">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
@@ -278,7 +280,7 @@
                               </div>
 
 
-                              <div class="col-sm-3  col-12 stretch-card grid-margin">
+                              <div class="col-sm-4  col-12 stretch-card grid-margin">
                                     <div class="card bg-secondary card-img-holder text-white">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
