2cb2e127c3029996c525a2f05976c834ab9ff7a5
Esther Eze <estherakowe@yahoo.com>
2024-09-19 14:51:17 +0100

    Update multistore-sidebar.blade.php

--- resources/views/layouts/multistore-sidebar.blade.php
+++ resources/views/layouts/multistore-sidebar.blade.php
@@ -16,7 +16,7 @@
                   <li class="nav-item">
                         <a class="nav-link" href="{{ url('') }}" target="">
                               <span class="menu-title">Outlet (s)</span>
-                              <i class="fa fa-chain  menu-icon fs-24"></i>
+                              <i class="mdi mdi-account-multiple  menu-icon fs-24"></i>
 
                         </a>
                   </li>
@@ -49,30 +49,6 @@
                         </div>
                   </li>
 
-
-                  <li class="nav-item">
-                        <a class="nav-link" href="{{ url('') }}" target="">
-                              <span class="menu-title">Sales (s)</span>
-                              <i class="menu-icon fs-24">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="18" height="18"
-                                          viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                          stroke-linecap="round" stroke-linejoin="round">
-                                          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                          <path
-                                                d="M9 14c0 1.657 2.686 3 6 3s6 -1.343 6 -3s-2.686 -3 -6 -3s-6 1.343 -6 3z">
-                                          </path>
-                                          <path d="M9 14v4c0 1.656 2.686 3 6 3s6 -1.344 6 -3v-4">
-                                          </path>
-                                          <path
-                                                d="M3 6c0 1.072 1.144 2.062 3 2.598s4.144 .536 6 0c1.856 -.536 3 -1.526 3 -2.598c0 -1.072 -1.144 -2.062 -3 -2.598s-4.144 -.536 -6 0c-1.856 .536 -3 1.526 -3 2.598z">
-                                          </path>
-                                          <path d="M3 6v10c0 .888 .772 1.45 2 2"></path>
-                                          <path d="M3 11c0 .888 .772 1.45 2 2"></path>
-                                    </svg>
-                              </i>
-
-                        </a>
-                  </li>
 
                   <li class="nav-item">
                         <a class="nav-link" href="{{ url('') }}" target="">
