21924b16099ec399ecdf02946446f5831fce6636
Esther Eze <estherakowe@yahoo.com>
2024-09-19 15:18:38 +0100

    Update all-outlets.blade.php

--- resources/views/multistore/parent/all-outlets.blade.php
+++ resources/views/multistore/parent/all-outlets.blade.php
@@ -23,11 +23,11 @@
                                                 <h4 class="font-weight-normal mb-3">Total Outlets
                                                       <i class="mdi mdi-pot-steam  mdi-24px float-end"></i>
                                                 </h4>
-                                                <h2 class="">{{$countVendor->count()}}</h2>
+                                                <h2 class="">{{$countChildVendor->count()}}</h2>
                                           </div>
                                           <hr class="w-100">
                                           <div class="card-body">
-                                                <h6 class="card-text">{{$countActivevendor->count()}} active </h6>
+                                                <h6 class="card-text">{{$countActiveChildVendor->count()}} active </h6>
                                           </div>
                                     </div>
                               </div>
@@ -104,27 +104,6 @@
                         </div>
                         @endif
 
-
-                        @if(session('setup-vendor'))
-                        <div class="alert  alert-success alert-dismissible" role="alert">
-                              <div class="d-flex">
-                                    <div>
-                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
-                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
-                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                <path
-                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
-                                                <path d="M12 9v4" />
-                                                <path d="M12 17h.01" />
-                                          </svg>
-                                    </div>
-                                    <div> {!! session('setup-vendor') !!}</div>
-                              </div>
-                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
-                        </div>
-                        @endif
                   </div>
             </div>
             <!---end Alert --->
@@ -155,7 +134,7 @@
                               Search:
                               <div class="ms-2 d-inline-block">
 
-                                    <form action="/parent-vendor" method="GET" role="search">
+                                    <form action="/outlets" method="GET" role="search">
                                           {{ csrf_field() }}
                                           <div class="input-group mb-2">
                                                 <input type="text" class="form-control" placeholder="Search for…"
