8eb25fa7b089b5ab9fbaaf20f99d65428bb5eb86
Esther Eze <estherakowe@yahoo.com>
2024-09-19 15:17:18 +0100

    Update all-outlets.blade.php

--- resources/views/multistore/parent/all-outlets.blade.php
+++ resources/views/multistore/parent/all-outlets.blade.php
@@ -8,7 +8,7 @@
       <div class="content-wrapper">
             <div class="page-header">
                   <h3 class="page-title">
-                <span class="text-info">{{$vendorName}}</span>  >>>  Outlet (s)
+                <span class="text-info">{{$username}}</span>  >>>  Outlet (s)
                   </h3>
                 
             </div>
