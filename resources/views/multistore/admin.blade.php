566918aa4a9112802a976ab2afb28fb8688f381d
Esther Eze <estherakowe@yahoo.com>
2024-09-19 14:47:00 +0100

    Update admin.blade.php

--- resources/views/multistore/admin.blade.php
+++ resources/views/multistore/admin.blade.php
@@ -3,4 +3,9 @@
 @extends('layouts.multistore-sidebar')
 @extends('layouts.footer')
 @section('content')
+
+<!--- Parent ---Dashboard---> 
+<!--- End -- Parent ---Dashboard---> 
+
+<!--- Child ---Dashboard---> 
 @endsection
