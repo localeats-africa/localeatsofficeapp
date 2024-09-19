6b02c28b80d9c9f830cd67b6a7dbe578af3ac348
Esther Eze <estherakowe@yahoo.com>
2024-09-19 18:26:04 +0100

    Update ParentVendorController.php

--- app/Http/Controllers/ParentVendorController.php
+++ app/Http/Controllers/ParentVendorController.php
@@ -102,5 +102,51 @@ class ParentVendorController extends Controller
             'role', 'countChildVendor', 'countActiveChildVendor', 'username', 'childVendor'));
     }
 
+    public function outletSupplies(Request $request, $store_id){
+        if(Auth::user()){
+            $name = Auth::user()->name;
+            $user_id = Auth::user()->id;
+            $role = DB::table('role')->select('role_name')
+            ->join('users', 'users.role_id', 'role.id')
+            ->where('users.id', $user_id)
+            ->pluck('role_name')->first();
+            $vendorName = DB::table('vendor')->where('id', $vendor_id)
+            ->select('*')->pluck('vendor_name')->first();
+
+            $perPage = $request->perPage ?? 25;
+            $search = $request->input('search');
+    
+            $foodMenu = DB::table('food_menu')
+            ->join('vendor', 'vendor.id', 'food_menu.vendor_id')
+            ->join('users', 'users.id', '=','food_menu.added_by')
+            ->where('food_menu.deleted_at', null)
+            ->where('food_menu.price', '!=', null)
+            ->where('food_menu.item', '!=', null)
+            ->where('food_menu.vendor_id', $vendor_id)
+            ->select(['vendor.vendor_name', 'food_menu.*', 'users.fullname'])
+            ->orderBy('food_menu.created_at', 'desc')
+            ->where(function ($query) use ($search) {  // <<<
+            $query->where('food_menu.item', 'LIKE', '%'.$search.'%')
+                    ->orWhere('food_menu.price', 'LIKE', '%'.$search.'%');
+            })
+            ->paginate($perPage,  $pageName = 'food')->appends(['per_page'   => $perPage]);
+            $pagination = $foodMenu->appends ( array ('search' => $search) );
+                if (count ( $pagination ) > 0){
+                    return view('vendormanager.vendor-food-menu',  compact(
+                    'perPage', 'name', 'role', 'foodMenu', 
+                    'vendorName'))->withDetails($pagination);     
+                } 
+            else{ 
+                // Session::flash('food-status', 'No record order found'); 
+                return view('vendormanager.vendor-food-menu',  compact(
+                'perPage', 'name', 'role', 'foodMenu', 
+                'vendorName'))->with('food-status', 'No record order found'); }
+            
+            return view('vendormanager.vendor-food-menu', compact('perPage', 'role', 
+            'name','foodMenu', 'vendorName'));
+
+
+        }
+    }
   
 }
