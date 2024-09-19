95f845953911e0258a5a308a6e5d4717de50d226
Esther Eze <estherakowe@yahoo.com>
2024-09-19 15:02:21 +0100

    Update ParentVendorController.php

--- app/Http/Controllers/ParentVendorController.php
+++ app/Http/Controllers/ParentVendorController.php
@@ -46,6 +46,61 @@ class ParentVendorController extends Controller
     }
 
     public function allChildVendor(Request $request){
+        $name = Auth::user()->name;
+        $user_id = Auth::user()->id;
+
+        $role = DB::table('role')->select('role_name')
+        ->join('users', 'users.role_id', 'role.id')
+        ->where('users.id', $user_id)
+        ->pluck('role_name')->first();
+
+        $parent = DB::table('multi_store')
+        ->where('user_id', $user_id)
+        ->get('*')->pluck('id')->first();
+
+        $countChildVendor =  DB::table('vendor')
+        ->join('sub_store', 'sub_store.vendor_id', 'vendor.id')
+        ->where('sub_store.multi_store_id', $parent)
+        ->get();
+         // a vendor is consider active if it's active on one or more platform
+        $countActiveChildVendor = DB::table('sales_platform')
+        ->join('vendor', 'vendor.id', '=', 'sales_platform.vendor_id')->distinct()
+        ->join('sub_store', 'sub_store.vendor_id', 'vendor.id')
+        ->where('sales_platform.vendor_status', 'active')
+        ->where('sub_store.multi_store_id', $parent)
+        ->get('sales_platform.vendor_id');
+      
+
+        $perPage = $request->perPage ?? 25;
+        $search = $request->input('search');
+
+        $childVendor = DB::table('vendor')
+        ->join('sub_store', 'sub_store.vendor_id', 'vendor.id')
+        ->join('state', 'state.id', '=', 'vendor.state_id')
+        ->where('sub_store.multi_store_id', $parent)
+        ->select(['vendor.id', 'vendor.vendor_status', 'vendor.store_area',
+        'vendor.store_name', 'state.state'])
+        ->orderBy('sub_store.created_at', 'desc')
+        ->where(function ($query) use ($search) {  // <<<
+        $query->where('vendor.store_name', 'LIKE', '%'.$search.'%')
+                ->orWhere('vendor.vendor_status', 'LIKE', '%'.$search.'%')
+                ->orWhere('vendor.store_area', 'LIKE', '%'.$search.'%');
+        })
+        ->paginate($perPage,  $pageName = 'outlets')->appends(['per_page'   => $perPage]);
+        $pagination = $childVendor->appends ( array ('search' => $search) );
+            if (count ( $pagination ) > 0){
+                return view('multistore.child-vendor',  compact(
+                'perPage', 'childVendor', 'role', 'vendorName', 
+                'countVendor', 'countActivevendor'))->withDetails($pagination);     
+            } 
+        else{ 
+            // Session::flash('food-status', 'No record order found'); 
+            return view('multistore.child-vendor',  compact('perPage', 
+            'childVendor', 'role', 'vendorName', 'countVendor', 'countActivevendor'))->with('food-status', 'No record order found'); }
+        
+            return view('multistore.child-vendor',  compact('perPage', 
+            'childVendor', 'role', 'vendorName', 'countVendor', 'countActivevendor'));
+    
       
         return view('multistore.parent.admin');
     
