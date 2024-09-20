251a8233bda8a80d31280c62671389f22717ada4
Esther Eze <estherakowe@yahoo.com>
2024-09-20 11:21:32 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -235,7 +235,7 @@ Route::controller(MultiVendorController::class)->group(function () {
 
 Route::controller(ParentVendorController::class)->group(function () {
     Route::get('{username}/outlets',  'allChildVendor')->name('outlets');
-    Route::get('outlet-supplies/{id}',  'outletSupplies')->name(' outlet-supplies');
+    Route::get('{username}/outlet-supplies/{id}',  'outletSupplies')->name(' outlet-supplies');
    
 });
 
