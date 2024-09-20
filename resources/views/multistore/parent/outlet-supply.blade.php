87f8c5639f71dd6be62f709177f9a9c66d77195c
Esther Eze <estherakowe@yahoo.com>
2024-09-20 11:42:45 +0100

    Update outlet-supply.blade.php

--- resources/views/multistore/parent/outlet-supply.blade.php
+++ resources/views/multistore/parent/outlet-supply.blade.php
@@ -4,5 +4,300 @@
 @extends('layouts.footer')
 @section('content')
 
+<!-- main-panel -->
+<div class="main-panel">
+      <div class="content-wrapper">
+            <div class="page-header">
+                  <h3 class="page-title">
+                        <span class="text-info">{{$outletStoreName}}</span> >>>> Supplies
+                  </h3>
+            </div>
 
+            <p></p>
+            <!--Alert here--->
+            <div class="row ">
+                  <div class="col-12">
+                        @if(session('expense-status'))
+                        <div class="alert alert-important alert-success alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                          </svg>
+                                    </div>
+                                    <div> {!! session('expense-status') !!}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
+
+
+                        @if(session('expense-error'))
+                        <div class="alert  alert-danger alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                          </svg>
+                                    </div>
+                                    <div> {!! session('expense-error') !!}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
+
+
+                  </div>
+            </div>
+            <!---end Alert --->
+
+            <p></p>
+            <div class="row ">
+                  <div class="col-12">
+                        <div class="card">
+                              <div class="card-header">
+                                    <h4 class="card-title"> </h4>
+                              </div>
+                              <div class="card-body border-bottom py-3">
+                                    <div class="d-flex">
+                                          <div class="text-secondary">
+                                                Show
+                                                <div class="mx-2 d-inline-block">
+                                                      <select id="pagination" class="form-control form-control-sm"
+                                                            name="perPage">
+                                                            <option value="5" @if($perPage==5) selected @endif>5
+                                                            </option>
+                                                            <option value="10" @if($perPage==10) selected @endif>
+                                                                  10
+                                                            </option>
+                                                            <option value="25" @if($perPage==25) selected @endif>
+                                                                  25
+                                                            </option>
+                                                            <option value="50" @if($perPage==50) selected @endif>
+                                                                  50
+                                                            </option>
+                                                      </select>
+                                                </div>
+                                                records
+                                          </div>
+                                          <div class="ms-auto text-secondary">
+                                                Search:
+                                                <div class="ms-2 d-inline-block">
+
+                                                      <form action="{{ route('add-expenses') }}" method="GET"
+                                                            role="search">
+                                                            {{ csrf_field() }}
+                                                            <div class="input-group mb-2">
+                                                                  <input type="text" class="form-control"
+                                                                        placeholder="Search for…" name="search">
+                                                                  <button type="submit" class="btn"
+                                                                        type="button">Go!</button>
+                                                            </div>
+                                                      </form>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
+
+                              <div class="table-responsive " id="card">
+                                    <table class="table table-striped card-table table-vcenter text-nowrap datatable"
+                                          id="orders">
+                                          <thead>
+                                                <tr>
+                                                      
+                                                      <th>Date</th>
+                                                      <th>Item</th>
+                                                      <th>Quantity</th>
+                                                </tr>
+                                          </thead>
+                                          <tbody>
+                                                @foreach($supply as $data)
+                                                <tr>
+                                                      <td>{{ date('d/m/Y', strtotime($data->created_at))}}</td>
+                                                      <td class="text-capitalize">{{$data->supply}}</td>
+                                                      <td>{{$data->supply_qty}}</td>
+
+
+                                                </tr>
+                                                @endforeach
+
+                                          </tbody>
+
+                                    </table>
+                              </div>
+                              <div class="card-footer d-flex align-items-center">
+                                    <p class="m-0 text-secondary">
+
+                                          Showing
+                                          {{ ($supply->currentPage() - 1) * $supply->perPage() + 1; }} to
+                                          {{ min($supply->currentPage()* $supply->perPage(), $supply->total()) }}
+                                          of
+                                          {{$supply->total()}} entries
+                                    </p>
+
+                                    <ul class="pagination m-0 ms-auto">
+                                          @if(isset($supply))
+                                          @if($supply->currentPage() > 1)
+                                          <li class="page-item ">
+                                                <a class="page-link text-danger"
+                                                      href="{{ $supply->previousPageUrl() }}" tabindex="-1"
+                                                      aria-disabled="true">
+                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
+                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M15 6l-6 6l6 6" />
+                                                      </svg>
+                                                      prev
+                                                </a>
+                                          </li>
+                                          @endif
+
+
+                                          <li class="page-item">
+                                                {{ $supply->appends(compact('perPage'))->links()  }}
+                                          </li>
+                                          @if($supply->hasMorePages())
+                                          <li class="page-item">
+                                                <a class="page-link text-danger" href="{{ $supply->nextPageUrl() }}">
+                                                      next
+                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
+                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M9 6l6 6l-6 6" />
+                                                      </svg>
+                                                </a>
+                                          </li>
+                                          @endif
+                                          @endif
+                                    </ul>
+                              </div>
+                        </div>
+                        <!--- card-->
+                  </div>
+            </div>
+      </div>
+      <!--- content wrapper---->
+      <!-- partial -->
+      <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                  <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©
+                        LocalEats Africa {{ date('Y')}} </a>. All rights
+                        reserved.</span>
+                  <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><i
+                              class="mdi mdi-heart text-danger"></i></span>
+            </div>
+      </footer>
+</div>
+<!-- main-panel -->
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
+
+<!-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+
+<!-- End custom js for this page -->
+<script type="text/javascript">
+$(document).ready(function() {
+      $("#item").select2({
+            placeholder: "Search ",
+            closeOnSelect: true,
+            language: {
+                  noResults: function(term) {
+                        return $(
+                              "<div>Result not found!. <a href='#' onclick='return myClick()'>click here add new</a></div>"
+                        );
+                  }
+            },
+      });
+
+      $('#btn-add-state').on("click", function() {
+            var newStateVal = $('#new-item').val();
+            // Set the value, creating a new option if necessary
+            // if ($('#item').find("option[value=" + newStateVal + "]").length) {
+            //       $('#item').val(newStateVal).trigger("change");
+            // } else {
+            // Create the DOM option that is pre-selected by default
+            var newState = new Option(newStateVal, newStateVal, true, true);
+            // Append it to the select
+            $('#item').append(newState).trigger('change').select2();
+            // }
+            var item = document.getElementById('item').value;
+            var vendor = document.getElementById('vendor').value;
+
+            var url = "{{ route('add-expenses-list') }}";
+            // url = showRoute;
+
+            //window.location = url;
+            $.ajaxSetup({
+                  headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                  }
+            });
+            $.ajax({
+                  method: 'POST',
+                  enctype: 'multipart/form-data',
+                  url: url,
+                  data: {
+                        //you can more data here
+                        'item': item,
+                        'vendor': vendor
+                  },
+                  success: function(data) {
+                        console.log(data.message);
+                        document.getElementById('response').style.display =
+                              '';
+                        document.getElementById('response').style.color =
+                              'green';
+                        document.getElementById('response').innerHTML = data
+                              .message;
+                        document.getElementById("show").style.display =
+                              'none';
+                  },
+                  error: function(data) {
+                        console.log(data);
+                  }
+            });
+      });
+});
+
+function myClick() {
+      var x = document.getElementById("show");
+      if (x.style.display === "none") {
+            x.style.display = "block";
+      } else {
+            x.style.display = "none";
+      }
+}
+</script>
+
+
+
+<script>
+$(function() {
+      $("#date").datepicker();
+});
+
+
+</script>
 @endsection
