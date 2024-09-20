6c7f4436de5ce0c0b5881b93bbed788c92cfe837
Esther Eze <estherakowe@yahoo.com>
2024-09-20 11:19:43 +0100

    Update outlet-supply.blade.php

---
+++ resources/views/multistore/parent/outlet-supply.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.head')
+@extends('layouts.header')
+@extends('layouts.multistore-sidebar')
+@extends('layouts.footer')
+@section('content')
+
+<!--- Parent ---Dashboard---> 
+<!--- End -- Parent ---Dashboard---> 
+hello
+
+<!--- Child ---Dashboard---> 
+@endsection
