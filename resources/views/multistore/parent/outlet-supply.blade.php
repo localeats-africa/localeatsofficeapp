7e7c44195468630f7fbffd3313f52c641fcb09cc
Esther Eze <estherakowe@yahoo.com>
2024-09-20 16:24:29 +0100

    Update outlet-supply.blade.php

--- resources/views/multistore/parent/outlet-supply.blade.php
+++ resources/views/multistore/parent/outlet-supply.blade.php
@@ -14,7 +14,7 @@
                   <nav aria-label="breadcrumb">
                 <ul class="breadcrumb">
                   <li class="breadcrumb-item active" aria-current="page">
-                    <span></span><a href="send-supplies/{{$vendor_id}}" class="btn btn-outline-danger">Click Here To Send Supplies </a>
+                    <span></span><a href="/send-supplies/{{$vendor_id}}" class="btn btn-outline-danger">Click Here To Send Supplies </a>
                   </li>
                 </ul>
               </nav>
