84405031f5de00f63c21323ccdd3b6c148f2812d
Esther Eze <estherakowe@yahoo.com>
2024-09-24 19:12:07 +0100

    Update send-supply.blade.php

--- resources/views/multistore/parent/send-supply.blade.php
+++ resources/views/multistore/parent/send-supply.blade.php
@@ -200,7 +200,7 @@ text-transform: lowercase;
                                                       <form action="{{ route('remove-supply-item') }}" method="post">
                                                       @csrf
                                                       {{csrf_field()}}
-                                                            <input type="text" value="{{$data->id}}">
+                                                            <input type="text" value="{{$data->id}}" name="id">
                                                             <button type="submit"  name="submit" class=" btn btn-xs text-danger"><i class="fa fa-trash"></i></button>
                                                       </form>
                                                     </td>
