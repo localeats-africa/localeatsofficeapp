f18fef80c1b1f769c96e3e4419164328f4f9fa2e
Esther Eze <estherakowe@yahoo.com>
2024-09-26 11:32:21 +0100

    Update VendorsController.php

--- app/Http/Controllers/VendorsController.php
+++ app/Http/Controllers/VendorsController.php
@@ -47,7 +47,9 @@ use Mail;
 class VendorsController extends Controller
 {
     //
-
+    public function __construct(){
+        $this->middleware(['auth', 'verified']);
+    }
     public function outletAllSupplies(){
 
     }
