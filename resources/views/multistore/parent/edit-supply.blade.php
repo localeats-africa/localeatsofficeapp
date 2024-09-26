712f8c304baf33f3b95a7c19c7b2ff995e221e30
Esther Eze <estherakowe@yahoo.com>
2024-09-26 15:48:41 +0100

    Update edit-supply.blade.php

--- resources/views/multistore/parent/edit-supply.blade.php
+++ resources/views/multistore/parent/edit-supply.blade.php
@@ -4,9 +4,73 @@
 @extends('layouts.footer')
 @section('content')
 
-<!--- Parent ---Dashboard---> 
-<!--- End -- Parent ---Dashboard---> 
-hello
+<!-- main-panel -->
+<div class="main-panel">
+      <div class="content-wrapper">
+            <div class="page-header">
+                  <h3 class="page-title">
+                       Edit <span class="text-info">{{$outletStoreName}}</span> >>>> Supply >>>  <span class="text-info">{{$outletStoreName}}</span> 
+                  </h3>
+                  <nav aria-label="breadcrumb">
+                        <ul class="breadcrumb">
+                              <li class="breadcrumb-item active" aria-current="page">
+                                    <span></span><a href="/{{$username}}/supply-to-outlet/{{$vendor_id}}"
+                                          class="btn btn-outline-danger">Click Here To Send Supplies </a>
+                              </li>
+                        </ul>
+                  </nav>
+            </div>
 
-<!--- Child ---Dashboard---> 
+            <p></p>
+            <!--Alert here--->
+            <div class="row ">
+                  <div class="col-12">
+                        @if(session('supply-status'))
+                        <div class="alert alert-important alert-success alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                          </svg>
+                                    </div>
+                                    <div> {!! session('supply-status') !!}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
+
+
+                        @if(session('supply-error'))
+                        <div class="alert  alert-danger alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                          </svg>
+                                    </div>
+                                    <div> {!! session('supply-error') !!}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
+
+
+                  </div>
+            </div>
+            <!---end Alert --->
+
 @endsection
