6429681ee369907619572c28bd6db31165bed810
Esther Eze <estherakowe@yahoo.com>
2024-09-26 17:51:39 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -246,7 +246,7 @@ Route::controller(ParentVendorController::class)->group(function () {
     Route::post('push-supplies',  'pushSupplies')->name('push-supplies');
    Route::post('remove-supply-item/{id}',  'deleteTempSupply')->name('remove-supply-item');
    Route::get('/{username}/edit-outlet-supply/{ref}',  'editOutletSupply')->name('edit-outlet-supply');
-    
+   Route::post('update-supplies',  'updateSupply')->name('update-supplies');
 });
 
 
