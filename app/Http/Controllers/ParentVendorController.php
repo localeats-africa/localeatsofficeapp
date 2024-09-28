c5b9e4450e6b122526d720773d95a0b6321dd4e6
Esther Eze <estherakowe@yahoo.com>
2024-09-28 13:27:33 +0100

    Update ParentVendorController.php

--- app/Http/Controllers/ParentVendorController.php
+++ app/Http/Controllers/ParentVendorController.php
@@ -507,40 +507,48 @@ class ParentVendorController extends Controller
         else{return redirect()->back()->with('error', 'Opps! something went wrong.'); }
     }
 
-    public function allFoodMenu(Request $request){
+    public function foodMenu(Request $request, $username){
         if(Auth::user()){
-            $name = Auth::user()->name;
+            $username = Auth::user()->username;
             $user_id = Auth::user()->id;
             $role = DB::table('role')->select('role_name')
             ->join('users', 'users.role_id', 'role.id')
             ->where('users.id', $user_id)
             ->pluck('role_name')->first();
 
-            $perPage = $request->perPage ?? 25;
+            $parentID = DB::table('multi_store')
+            ->where('user_id',  $id)
+            ->get()->pluck('id')->first();
+
+            $perPage = $request->perPage ?? 15;
             $search = $request->input('search');
     
-            $foodMenu = DB::table('food_menu')
-            ->join('vendor', 'vendor.id', '=','food_menu.vendor_id')
-            ->join('users', 'users.id', '=','food_menu.added_by')
-            ->where('food_menu.deleted_at', null)
-            ->where('food_menu.price', '!=', null)
-            ->where('food_menu.item', '!=', null)
-            ->select(['vendor.vendor_name', 'food_menu.*', 'users.fullname'])
-            ->orderBy('food_menu.created_at', 'desc')
+            $foodMenu = DB::table('vendor_food_menu')
+            ->where('deleted_at', null)
+            ->where('store_id', $parentID)
+            ->where('deleted_at', null)
+            ->where('price', '!=', null)
+            ->where('food_item', '!=', null)
+            ->select(['*'])
+            ->orderBy('created_at', 'desc')
             ->where(function ($query) use ($search) {  // <<<
-            $query->where('vendor.vendor_name', 'LIKE', '%'.$search.'%')
-                ->orWhere('food_menu.item', 'LIKE', '%'.$search.'%')
-                    ->orWhere('food_menu.price', 'LIKE', '%'.$search.'%');
+            $query->where('food_item', 'LIKE', '%'.$search.'%')
+                ->orWhere('category', 'LIKE', '%'.$search.'%')
+                    ->orWhere('price', 'LIKE', '%'.$search.'%');
             })
             ->paginate($perPage,  $pageName = 'food')->appends(['per_page'   => $perPage]);
             $pagination = $foodMenu->appends ( array ('search' => $search) );
                 if (count ( $pagination ) > 0){
-                    return view('vendormanager.all-food-menu',  compact(
-                    'perPage', 'name', 'role', 'foodMenu'))->withDetails($pagination);     
+                    return view('multistore.parent.vendor-food-menu',  compact(
+                    'perPage', 'username', 'role', 'foodMenu', 'parentID'))->withDetails($pagination);     
                 } 
-            else{return redirect()->back()->with('food-status', 'No record order found'); }
-            
-            return view('vendormanager.all-food-menu', compact('perPage', 'role', 'name','foodMenu', 'vendor'));
+            else{
+                //return redirect()->back()->with('food-status', 'No record order found');
+                return view('multistore.parent.vendor-food-menu',  compact(
+                    'perPage', 'username', 'role', 'foodMenu', 'parentID'));
+            }
+            return view('multistore.parent.vendor-food-menu',  compact(
+                'perPage', 'username', 'role', 'foodMenu', 'parentID'));
         }
     }
     public function addFoodMenu(Request $request){
