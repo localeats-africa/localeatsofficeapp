21ade0381ebbfff0c3aa8d76f1c520846455d03e
Esther Eze <estherakowe@yahoo.com>
2024-09-29 12:14:48 +0100

    Update instore-sales.blade.php

---
+++ resources/views/multistore/child/instore-sales.blade.php
@@ -0,0 +1,283 @@
+@extends('layouts.head')
+@extends('layouts.header')
+@extends('layouts.multistore-sidebar')
+@extends('layouts.footer')
+@section('content')
+
+<!-- main-panel -->
+<div class="main-panel">
+      <div class="content-wrapper">
+            <div class="page-header">
+                  <h3 class="page-title">
+                        <span class="text-info">{{$storeName}}</span> >>>> In-store Sales
+                  </h3>
+                  <nav aria-label="breadcrumb">
+                        <ul class="breadcrumb">
+                              <li class="breadcrumb-item active" aria-current="page">
+                                    <span></span><a href="/{{$username}}/new-sales"
+                                          class="btn btn-outline-danger">Add New Sales </a>
+                              </li>
+                        </ul>
+                  </nav>
+            </div>
+
+            <p></p>
+            <!--Alert here--->
+            <div class="row ">
+                  <div class="col-12">
+                        @if(session('sales-status'))
+                        <div class="alert alert-important alert-success alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                          </svg>
+                                    </div>
+                                    <div> {!! session('sales-status') !!}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
+
+
+                        @if(session('sales-error'))
+                        <div class="alert  alert-danger alert-dismissible" role="alert">
+                              <div class="d-flex">
+                                    <div>
+                                          <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                          <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                      d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                          </svg>
+                                    </div>
+                                    <div> {!! session('sales-error') !!}</div>
+                              </div>
+                              <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                        </div>
+                        @endif
+
+
+                  </div>
+            </div>
+            <!---end Alert --->
+
+            <p></p>
+            <div class="row ">
+                  <div class="col-12">
+                        <div class="card">
+                              <div class="card-header">
+                                    <h4 class="card-title"> </h4>
+                              </div>
+                              <div class="card-body border-bottom py-3">
+                                    <div class="d-flex">
+                                          <div class="text-secondary">
+                                                Show
+                                                <div class="mx-2 d-inline-block">
+                                                      <select id="pagination" class="form-control form-control-sm"
+                                                            name="perPage">
+                                                            <option value="5" @if($perPage==5) selected @endif>5
+                                                            </option>
+                                                            <option value="10" @if($perPage==10) selected @endif>
+                                                                  10
+                                                            </option>
+                                                            <option value="25" @if($perPage==25) selected @endif>
+                                                                  25
+                                                            </option>
+                                                            <option value="50" @if($perPage==50) selected @endif>
+                                                                  50
+                                                            </option>
+                                                      </select>
+                                                </div>
+                                                records
+                                          </div>
+                                          <div class="ms-auto text-secondary">
+                                                Search:
+                                                <div class="ms-2 d-inline-block">
+                                                      <form action="/{{$username}}/outlet-supplies/{{$vendor_id}}"
+                                                            method="GET" role="search">
+                                                            {{ csrf_field() }}
+                                                            <div class="input-group mb-2">
+                                                                  <input type="text" class="form-control"
+                                                                        placeholder="Search for…" name="search">
+                                                                  <button type="submit" class="btn"
+                                                                        type="button">Go!</button>
+                                                            </div>
+                                                      </form>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
+
+                              <div class="table-responsive " id="card">
+                                    <table class="table table-striped card-table table-center text-nowrap datatable"
+                                          id="orders">
+                                          <thead>
+                                                <tr>
+                                                      <th></th>
+                                                      <th>Date</th>
+                                                      <th>Category</th>
+                                                      <th>Item</th>
+                                                      <th>Unit Price</th>
+                                                      <th>QTY</th>
+                                                      <th>Amount</th>
+                                                </tr>
+                                          </thead>
+                                          <tbody>
+                                                @foreach($sales as $data)
+                                                <tr>
+
+                                                      <td> <a class="text-danger"
+                                                                  href="/{{$username}}/supplies-receipt/{{$data->supply_ref}}"
+                                                                  title="view">
+                                                                 <i class="fa fa-eye"></i> view
+                                                            </a></td>
+                                                      <td>{{ date('d/m/Y', strtotime($data->created_at))}}</td>
+                                                      <td class="text-capitalize">{{$data->category}}</td>
+                                                      <td class="text-capitalize">{{$data->food_item}}</td>
+                                                      <td>{{$data->price}}</td>
+                                                      <td>{{$data->quantity}}</td>
+                                                      <td>{{$data->amount}}</td>
+                                                      <td></td>
+
+                                                </tr>
+                                                @endforeach
+
+                                          </tbody>
+
+                                    </table>
+                              </div>
+                              <div class="card-footer d-flex align-items-center">
+                                    <p class="m-0 text-secondary">
+
+                                          Showing
+                                          {{ ($sales->currentPage() - 1) * $sales->perPage() + 1; }} to
+                                          {{ min($sales->currentPage()* $sales->perPage(), $sales->total()) }}
+                                          of
+                                          {{$sales->total()}} entries
+                                    </p>
+
+                                    <ul class="pagination m-0 ms-auto">
+                                          @if(isset($sales))
+                                          @if($sales->currentPage() > 1)
+                                          <li class="page-item ">
+                                                <a class="page-link text-danger" href="{{ $sales->previousPageUrl() }}"
+                                                      tabindex="-1" aria-disabled="true">
+                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
+                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M15 6l-6 6l6 6" />
+                                                      </svg>
+                                                      prev
+                                                </a>
+                                          </li>
+                                          @endif
+
+
+                                          <li class="page-item">
+                                                {{ $sales->appends(compact('perPage'))->links()  }}
+                                          </li>
+                                          @if($sales->hasMorePages())
+                                          <li class="page-item">
+                                                <a class="page-link text-danger" href="{{ $sales->nextPageUrl() }}">
+                                                      next
+                                                      <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
+                                                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M9 6l6 6l-6 6" />
+                                                      </svg>
+                                                </a>
+                                          </li>
+                                          @endif
+                                          @endif
+                                    </ul>
+                              </div>
+                        </div>
+                        <!--- card-->
+                  </div>
+            </div>
+      </div>
+      <!--- content wrapper---->
+      <!-- partial -->
+      <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                  <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©
+                        LocalEats Africa {{ date('Y')}} </a>. All rights
+                        reserved.</span>
+                  <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><i
+                              class="mdi mdi-heart text-danger"></i></span>
+            </div>
+      </footer>
+</div>
+<!-- main-panel -->
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
+
+<!-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+
+<!-- End custom js for this page -->
+
+<script type="text/javascript">
+function updateQTY() {
+      document.getElementById('response').style.display = 'none';
+      var id = document.getElementById('id').value;
+      var quantity = document.getElementById('supply_qty').value;
+      var showRoute = "{{ route('update-supply-quantity', ':id') }}";
+      url = showRoute.replace(':id', id);
+
+      //window.location = url;
+      $.ajaxSetup({
+            headers: {
+                  'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+      });
+      $.ajax({
+            method: 'PATCH',
+            enctype: 'multipart/form-data',
+            url: url,
+            data: {
+                  //you can more data here
+                  'quantity': quantity,
+                  'id': id
+            },
+            success: function(data) {
+                  console.log(data.message);
+                  document.getElementById('response').style.display = '';
+                  document.getElementById('response').style.color = 'green';
+                  document.getElementById('response').innerHTML = data.message;
+            },
+            error: function(data) {
+                  console.log(data);
+            }
+      });
+
+}
+</script>
+
+
+
+<script>
+$(function() {
+      $("#date").datepicker();
+});
+</script>
+@endsection
