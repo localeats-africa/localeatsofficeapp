167032855696531ef281cd6bbb95bd5d020738b1
Esther Eze <estherakowe@yahoo.com>
2024-09-29 22:53:19 +0100

    Update multistore-sidebar.blade.php

--- resources/views/layouts/multistore-sidebar.blade.php
+++ resources/views/layouts/multistore-sidebar.blade.php
@@ -50,7 +50,7 @@
                   <!-- child vendor ----> 
                   @if(Auth::user()->role_id == '10')
                   <li class="nav-item">
-                        <a class="nav-link" href="{{ url('v', [auth()->user()->username] ) }}">
+                  <a class="nav-link" href="{{ url('/', [auth()->user()->username, 'vendor'] ) }}">
                               <span class="menu-title">Dashboard</span>
                               <i class="mdi mdi-home menu-icon"></i>
                         </a>
