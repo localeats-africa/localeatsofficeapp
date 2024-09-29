3b08a35ff403bfded9683a7d0f8d8443e9e24711
Esther Eze <estherakowe@yahoo.com>
2024-09-29 10:29:24 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -235,11 +235,14 @@ Route::controller(MultiVendorController::class)->group(function () {
    //autocomplete
    Route::group(['middleware' => ['only.ajax']], function() {
         Route::get('autocomplete-expenses/{vendor_id}', 'autocompleteExpenses')->name('autocomplete-expenses');
+        Route::get('autocomplete-vendor-food-menu/{vendor_id}', 'autocompleteFoodMenu')->name('autocomplete-vendor-food-menu');
+        
     }); 
     Route::post('add-outlet-expenses', 'storeVendorDailyExpenses')->name('add-outlet-expenses');
     Route::post('add-instore-sales',  'addInstoreSales')->name('add-instore-sales');
     Route::get('/{username}/instore-sales',  'InStoreSales')->name('instore-sales');
     Route::get('/{username}/new-sales',  'newInStoreSales')->name('new-sales');
+
 });
 
 Route::controller(ParentVendorController::class)->group(function () {
