e7924eb85823a09bfdc648f6e5d0d06b9177ef2d
Esther Eze <estherakowe@yahoo.com>
2024-09-29 07:21:14 +0100

    Update multistore-sidebar.blade.php

--- resources/views/layouts/multistore-sidebar.blade.php
+++ resources/views/layouts/multistore-sidebar.blade.php
@@ -38,7 +38,7 @@
                   <li class="nav-item">
                         <a class="nav-link" href="{{ url(auth()->user()->username, 'expenses-category') }}" target="">
                               <span class="menu-title">Expenses  Category (s)</span>
-                              <i class="mdi mdi-food-fork-drink  menu-icon fs-24"></i>
+                              <i class="mdi mdi-cash-minus menu-icon fs-24"></i>
 
                         </a>
                   </li>
