4d53b7d679a19fc9534b194d78b0dba10f16fd12
Esther Eze <estherakowe@yahoo.com>
2024-10-05 15:03:08 +0100

    Update admin.blade.php

--- resources/views/multistore/parent/admin.blade.php
+++ resources/views/multistore/parent/admin.blade.php
@@ -187,7 +187,7 @@
                                                 <h4 class="font-weight-normal">Glovo Sales<i
                                                             class="mdi mdi-cloud-braces mdi-24px float-end"></i>
                                                 </h4>
-                                                <h2 class="mb-5">₦{{0}}</h2>
+                                                <h2 class="mb-5">₦{{$sumGlovoOrder}}</h2>
                                                 <hr class="w-100">
                                                 <h6 class="card-text">From <span class="text-dark">(
                                                             {{$GlovoOrderCount}} )</span>
