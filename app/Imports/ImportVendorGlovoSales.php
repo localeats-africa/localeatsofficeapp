6294d3cd77f4853262002064fa76d567de1f6d80
Esther Eze <estherakowe@yahoo.com>
2024-10-05 14:21:13 +0100

    Update ImportVendorGlovoSales.php

--- app/Imports/ImportVendorGlovoSales.php
+++ app/Imports/ImportVendorGlovoSales.php
@@ -7,8 +7,11 @@ use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\DB;
 
-class ImportVendorGlovoSales implements ToModel, WithHeadingRow
+use Auth;
+
+class ImportVendorGlovoSales implements ToModel
 {
     /**
     * @param Collection $collection
@@ -28,21 +31,21 @@ class ImportVendorGlovoSales implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         return new VendorGlovoImportSales([
-            'a'     => $row['a'],
-            'b'     => $row['b'],
-            'c'     => $row['c'],
-            'e'     => $row['e'],
-            'f'     => $row['f'],
-            'g'     => $row['g'],
-            'h'     => $row['h'],
-            'i'     => $row['i'],
-            'j'     => $row['j'],
-            'k'     => $row['k'],
-            'l'     => $row['l'],
-            'm'     => $row['m'],
-            'n'     => $row['n'],
-            'o'     => $row['o'],
-            'p'     => $row['p'],
+            'a'     => $row[0],
+            'b'     => $row[1],
+            'c'     => $row[2],
+            'e'     => $row[3],
+            'f'     => $row[4],
+            'g'     => $row[5],
+            'h'     => $row[6],
+            'i'     => $row[7],
+            'j'     => $row[8],
+            'k'     => $row[9],
+            'l'     => $row[10],
+            'm'     => $row[11],
+            'n'     => $row[12],
+            'o'     => $row[13],
+            'p'     => $row[14],
             'added_by' => Auth::user()->id,
             'parent_id'     => $this->platform_id,
             'vendor_id'     => $this->vendor_id,
