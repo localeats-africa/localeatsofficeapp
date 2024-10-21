9092971027d6a9a40da35034d89bcea7725810eb
Esther Eze <estherakowe@yahoo.com>
2024-10-21 16:09:24 +0100

    Update all-orders.blade.php

--- resources/views/admin/all-orders.blade.php
+++ resources/views/admin/all-orders.blade.php
@@ -150,6 +150,125 @@
             </div>
             <!---end Alert --->
             <p></p>
+            <form method="post" action="{{ route('setup') }}" name="submit" enctype="multipart/form-data">
+                  @csrf
+                  {{csrf_field()}}
+                  <div class="row">
+                        <div class="col-md-4 grid-margin stretch-card">
+                              <div class="card">
+                                    <div class="card-body">
+                                          <div class="form-group">
+
+                                                <label>Vendor <i class="text-danger">*</i></label>
+                                                <select class="js-example-basic-single text-secondary"
+                                                      style="width:100%" name="vendor" id="vendor">
+                                                      <option> Search
+                                                      </option>
+                                                      @foreach($vendor as $data)
+                                                      <option value="{{$data->id}}">
+                                                            {{$data->vendor_name}}</option>
+                                                      @endforeach
+                                                </select>
+
+                                          </div>
+                                          @error('vendor')
+                                          <div class="alert alert-danger alert-dismissible" role="alert">
+                                                <div class="d-flex">
+                                                      <div>
+                                                            <!-- Download SVG icon from http://tabler-icons.io/i/alert-circle -->
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                  class="icon alert-icon" width="24" height="24"
+                                                                  viewBox="0 0 24 24" stroke-width="2"
+                                                                  stroke="currentColor" fill="none"
+                                                                  stroke-linecap="round" stroke-linejoin="round">
+                                                                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                                  <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                                                                  <path d="M12 8v4" />
+                                                                  <path d="M12 16h.01" />
+                                                            </svg>
+                                                      </div>
+                                                      <div>
+                                                            {{ $message }}
+                                                      </div>
+                                                </div>
+                                                <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                                          </div>
+                                          @enderror
+                                    </div>
+                              </div>
+                        </div>
+
+
+                        <div class="col-md-4 grid-margin stretch-card">
+                              <div class="card">
+                                    <div class="card-body">
+                                          <div class="form-label ">Reference / Code <i class="text-danger">*</i>
+                                          </div>
+                                          <input type="text" class="form-control" name="reference">
+                                          @error('reference')
+                                          <div class="alert alert-danger alert-dismissible" role="alert">
+                                                <div class="d-flex">
+                                                      <div>
+                                                            <!-- Download SVG icon from http://tabler-icons.io/i/alert-circle -->
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                  class="icon alert-icon" width="24" height="24"
+                                                                  viewBox="0 0 24 24" stroke-width="2"
+                                                                  stroke="currentColor" fill="none"
+                                                                  stroke-linecap="round" stroke-linejoin="round">
+                                                                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                                  <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                                                                  <path d="M12 8v4" />
+                                                                  <path d="M12 16h.01" />
+                                                            </svg>
+                                                      </div>
+                                                      <div>
+                                                            {{ $message }}
+                                                      </div>
+                                                </div>
+                                                <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                                          </div>
+                                          @enderror
+                                    </div>
+                              </div>
+                        </div>
+
+
+                  </div>
+                  <!--- row---->
+
+                  <!-- row -->
+                  <div class="row">
+                        <div class="col-md-6">
+                        </div>
+                        <div class="col-md-6 col-12 grid-margin stretch-card justify-content-end">
+
+                              <!-- send button here -->
+                              <div class="card-footer bg-transparent mt-auto">
+                                    <div class="btn-list ">
+                                          <button type="submit" name="submit"
+                                                class="btn bg-gradient-primary  text-white">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                      class="icon icon-tabler icon-tabler-device-floppy" width="24"
+                                                      height="24" viewBox="0 0 24 24" stroke-width="1.5"
+                                                      stroke="currentColor" fill="none" stroke-linecap="round"
+                                                      stroke-linejoin="round">
+                                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                      <path
+                                                            d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" />
+                                                      <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                                      <path d="M14 4l0 4l-6 0l0 -4" />
+                                                </svg>
+                                                Submit
+                                          </button>
+                                    </div>
+                              </div>
+
+                        </div>
+                  </div>
+                  <!-- row -->
+            </form>
+
+            <p></p>
             <div class="row ">
                   <div class="col-12">
                         <div class="card">
