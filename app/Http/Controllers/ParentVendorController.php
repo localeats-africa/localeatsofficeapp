75c601bcb8413860eb4516de59b13cadcf996d67
Esther Eze <estherakowe@yahoo.com>
2024-10-24 08:30:52 +0100

    Update ParentVendorController.php

--- app/Http/Controllers/ParentVendorController.php
+++ app/Http/Controllers/ParentVendorController.php
@@ -780,14 +780,15 @@ class ParentVendorController extends Controller
         ->where('sub_store.multi_store_id', $parentID)
         ->get('vendor.*');
 
-        $salesChannel = 
-        DB::table('sales_platform')
+        $salesChannel =  DB::table('sales_platform')
         ->join('sub_store', 'sub_store.vendor_id', '=', 'sales_platform.vendor_id')
         ->where('sales_platform.vendor_status', 'active')
         ->where('sub_store.multi_store_id', $parentID)
         ->distinct('platform_name')
         ->get('sales_platform.platform_name');
         //Platforms::all();
+
+       // dd( $salesChannel );
 
         $perPage = $request->perPage ?? 10;
         $search = $request->input('search');
