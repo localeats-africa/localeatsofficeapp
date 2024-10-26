8cf4603f13fea614c7987fc85c7ac6589a6fafb7
Esther Eze <estherakowe@yahoo.com>
2024-10-26 12:04:05 +0100

    Update admin.blade.php

--- resources/views/multistore/parent/admin.blade.php
+++ resources/views/multistore/parent/admin.blade.php
@@ -220,7 +220,7 @@
                                                 <h4 class="font-weight-normal">Bertatech Sales <i
                                                             class="mdi mdi-cloud-braces mdi-24px float-end"></i>
                                                 </h4>
-                                                <h2 class="mb-5">₦{{0}}</h2>
+                                                <h2 class="mb-5">₦{{$btsCommission}}</h2>
                                                 <hr class="w-100">
                                           </div>
                                     </div>
