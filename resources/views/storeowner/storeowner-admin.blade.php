d3e1e1e289ffc77f66c900cb739822aaf1dda67c
Esther Eze <estherakowe@yahoo.com>
2024-10-30 16:08:30 +0100

    Update storeowner-admin.blade.php

--- resources/views/storeowner/storeowner-admin.blade.php
+++ resources/views/storeowner/storeowner-admin.blade.php
@@ -81,69 +81,68 @@
                   </form>
             </div>
             <!---end row --->
-       
-     <!--row-->
-     <div class="row row-cards">
-                        <div class="col-md-4 stretch-card grid-margin">
-                                    <div class="card bg-secondary card-img-holder text-dark">
-                                          <div class="card-body">
-                                                <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
-                                                      class="card-img-absolute" alt="circle-image">
-                                                <h4 class="font-weight-normal">In-Store Sales<i
-                                                            class="fa fa-cutlery  mdi-24px float-end"></i>
-                                                </h4>
-                                                <h2 class="mb-5">{{number_format($offlineSales)}}</h2>
-                                                <hr class="w-100">
-                                                
-                                          </div>
-                                    </div>
+
+            <!--row-->
+            <div class="row row-cards">
+                  <div class="col-md-4 stretch-card grid-margin">
+                        <div class="card bg-secondary card-img-holder text-dark">
+                              <div class="card-body">
+                                    <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
+                                          class="card-img-absolute" alt="circle-image">
+                                    <h4 class="font-weight-normal">Total In-Store Sales<i
+                                                class="fa fa-cutlery  mdi-24px float-end"></i>
+                                    </h4>
+                                    <h2 class="mb-5">₦{{number_format($offlineSales)}}</h2>
+                                    <hr class="w-100">
+
+                              </div>
+                        </div>
+                  </div>
+
+                  <div class="col-sm-4 col-12 stretch-card grid-margin">
+                        <div class="card bg-gradient-secondary card-img-holder text-dark">
+                              <div class="card-body">
+                                    <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
+                                          class="card-img-absolute" alt="circle-image">
+                                    <h4 class="font-weight-normal">Total Onilne Sales
+                                          <i class="mdi mdi-cash mdi-24px float-end"></i>
+                                    </h4>
+                                    <h2 class="mb-5">₦{{number_format($payouts)}}</h2>
+                                    <hr class="w-100">
+                              </div>
+                        </div>
+                  </div>
+
+                  <div class="col-md-4 stretch-card grid-margin">
+                        <div class="card bg-secondary card-img-holder text-dark">
+                              <div class="card-body">
+                                    <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
+                                          class="card-img-absolute" alt="circle-image">
+                                    <h4 class="font-weight-normal">Expenses
+                                          <i class="mdi mdi-cash mdi-24px float-end"></i>
+                                    </h4>
+                                    <h2 class="mb-5">₦{{number_format($expenses)}}</h2>
+                                    <hr class="w-100">
+
                               </div>
 
-                              <div class="col-sm-4 col-12 stretch-card grid-margin">
-                                    <div class="card bg-gradient-secondary card-img-holder text-dark">
-                                          <div class="card-body">
-                                                <img src="{{ asset('assets/images/dashboard/circle.svg')}}"
-                                                      class="card-img-absolute" alt="circle-image">
-                                                <h4 class="font-weight-normal">Total Onilne Sales
-                                                      <i class="mdi mdi-cash mdi-24px float-end"></i>
-                                                </h4>
-                                                <h2 class="mb-5">₦{{number_format($payouts, 2)}}</h2>
-                                                <hr class="w-100">
-                                          </div>
-                                    </div>
-                              </div>
+                        </div>
+                  </div>
+
+            </div>
+            <!--row--->
+
+            <div class="row ">
+                  <div class="col-12">
+                        <div class="row row-cards">
 
                               <div class="col-md-4 stretch-card grid-margin">
                                     <div class="card bg-secondary card-img-holder text-dark">
                                           <div class="card-body">
                                                 <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
                                                       class="card-img-absolute" alt="circle-image">
-                                                <h4 class="font-weight-normal">Expenses   
-                                                      <i class="mdi mdi-cash mdi-24px float-end"></i>
-                                                </h4>
-                                                <h2 class="mb-5">{{number_format($expenses)}}</h2>
-                                                <hr class="w-100">
-                                                
-                                          </div>
-
-                                    </div>
-                              </div>
-
-                        </div>
-                        <!--row--->
-
-            <div class="row ">
-                  <div class="col-12">
-                        <div class="row row-cards">
-
-                        <div class="col-md-4 stretch-card grid-margin">
-                                    <div class="card bg-secondary card-img-holder text-dark">
-                                          <div class="card-body">
-                                                <img src="{{ asset('assets/images/dashboard/circle.svg') }}"
-                                                      class="card-img-absolute" alt="circle-image">
-                                                <h4 class="font-weight-normal">Profit/Loss 
-                                                <i
-                                                            class="mdi mdi-24px float-end">
+                                                <h4 class="font-weight-normal">Profit/Loss
+                                                      <i class="mdi mdi-24px float-end">
                                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon"
                                                                   width="24" height="24" viewBox="0 0 24 24"
                                                                   stroke-width="2" stroke="currentColor" fill="none"
@@ -164,7 +163,7 @@
                                                             </svg>
                                                       </i>
                                                 </h4>
-                                                <h2 class="mb-5">{{$countAllOrder}}</h2>
+                                                <h2 class="mb-5">₦{{number_format($profitLoss)}}</h2>
                                                 <hr class="w-100">
 
                                           </div>
@@ -204,7 +203,6 @@
                                     </div>
                               </div>
 
-
                         </div>
                         <!--row--->
                   </div>
@@ -212,46 +210,7 @@
             <!--row-deck-->
 
       </div>
-</div>
-<!--row-deck-->
-<!-- </div> -->
-<p></p>
-<!--Alert here--->
-<!-- <div class="container "> -->
-<div class="row ">
-      <div class="col-12">
-            @if(session('add-vendor'))
-            <div class="alert alert-important alert-success alert-dismissible" role="alert">
-                  <div class="d-flex">
-                        <div>
-                              <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
-                              <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24"
-                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                    stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path
-                                          d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
-                                    <path d="M12 9v4" />
-                                    <path d="M12 17h.01" />
-                              </svg>
-                        </div>
-                        <div> {!! session('add-vendor') !!}</div>
-                  </div>
-                  <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
-            </div>
-            @endif
-
-
-      </div>
-</div>
-
-<!---end Alert --->
-
-<!--- row--->
-<p></p>
-<!-- </div> -->
-</div>
-<!--content wrapper-->
+      <!--content wrapper-->
 <footer class="footer">
       <div class="d-sm-flex justify-content-center justify-content-sm-between">
             <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©
