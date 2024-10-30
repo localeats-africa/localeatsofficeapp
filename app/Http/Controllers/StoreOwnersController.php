f3ae13c980f582c3bf86060d4d3477c885a9b2a4
Esther Eze <estherakowe@yahoo.com>
2024-10-30 12:27:18 +0100

    Update StoreOwnersController.php

--- app/Http/Controllers/StoreOwnersController.php
+++ app/Http/Controllers/StoreOwnersController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use App\Models\SalesPlatform;
+use App\Models\Platforms;
+use App\Models\BankList;
+use App\Models\FoodType;
+use App\Models\FoodMenu;
+use App\Models\RestaurantType;
+use App\Models\Vendor;
+use App\Models\Orders;
+use App\Models\Area;
+use App\Models\State;
+use App\Models\Role;
+use App\Models\Level;
+use App\Models\Commission;
+use App\Mail\NewUserEmail;
+use App\Models\ExpensesList;
+use App\Models\OfflineSales;
+use App\Models\VendorExpenses;
+use App\Models\OfflineFoodMenu;
+use App\Models\MultiStoreRole;
+use App\Models\MultiStore;
+use App\Models\SubStore;
+use App\Models\VendorInventory;
+use App\Models\SubVendorInventory;
+use App\Models\TempVendorInventory;
+use App\Models\InventoryItemSizes;
+use App\Models\VendorInstoreSales;
+use App\Models\FoodCategory;
+use App\Models\VendorFoodMenu;
+use App\Models\VendorExpensesCategory;
+use App\Models\VendorOnlineSales;
+
+use Excel;
+use Auth;
+use Session;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Mail;
+
 
 class StoreOwnersController extends Controller
 {
     //
+    public function __construct(){
+        $this->middleware(['auth', 'user-access:11', 'verified']);
+    }
+
+    public function storeowner(Request $request){
+
+    }
 }
