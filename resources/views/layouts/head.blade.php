dbffcb56df6ffd1ca2dc3c8cc6721ccd92be8345
Esther Eze <estherakowe@yahoo.com>
2024-10-30 18:06:16 +0100

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -13,7 +13,17 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
       <!----tinymyn ---> 
-      <link href="https://cdn.jsdelivr.net/npm/quill@2/dist/quill.core.css" rel="stylesheet" />
+    
+      <link
+  href="https://cdn.jsdelivr.net/npm/quill@2/dist/quill.snow.css"
+  rel="stylesheet"
+/>
+<link
+  href="https://cdn.jsdelivr.net/npm/quill@2/dist/quill.bubble.css"
+  rel="stylesheet"
+/>
+
+
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
