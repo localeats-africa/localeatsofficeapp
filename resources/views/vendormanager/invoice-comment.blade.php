6d14a06fc3e7204492fbbb5aec8bf058ca7613b1
Esther Eze <estherakowe@yahoo.com>
2024-10-30 17:48:46 +0100

    Update invoice-comment.blade.php

--- resources/views/vendormanager/invoice-comment.blade.php
+++ resources/views/vendormanager/invoice-comment.blade.php
@@ -6,13 +6,14 @@
 
 
 @push('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.2.0/tinymce.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
+
 <script>
 tinymce.init({
     selector: 'textarea#description',
     menubar: false,
     plugins: 'code table lists image',
-    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | table | image',
+    toolbar: 'undo redo | blocks | bold italic |',
 });
 </script>
 @endpush
