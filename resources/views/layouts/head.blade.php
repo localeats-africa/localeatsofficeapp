f7333c521478331f54da0e197449387c5c9aa4b3
Esther Eze <estherakowe@yahoo.com>
2024-10-30 18:06:34 +0100

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -23,6 +23,8 @@
   rel="stylesheet"
 />
 
+<script src="https://cdn.jsdelivr.net/npm/quill@2/dist/quill.js"></script>
+
 
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
