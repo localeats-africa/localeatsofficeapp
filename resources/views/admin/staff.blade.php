dd53d3246ea2f5678928ec7fd27f8c37f9e0ce48
Esther Eze <estherakowe@yahoo.com>
2024-10-31 17:34:52 +0100

    Update staff.blade.php

--- resources/views/admin/staff.blade.php
+++ resources/views/admin/staff.blade.php
@@ -277,7 +277,7 @@
                                                       <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
                                                       <path d="M14 4l0 4l-6 0l0 -4" />
                                                 </svg>
-                                                Add Staff
+                                                Add User
                                           </button>
                                     </div>
                               </div>
