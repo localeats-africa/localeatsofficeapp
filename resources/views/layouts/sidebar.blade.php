1fb01033bfc798809d076cc6f31e8c19b0af992c
Esther Eze <estherakowe@yahoo.com>
2024-11-05 09:17:16 +0100

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -100,7 +100,7 @@
 
                   <li class="nav-item">
                         <a class="nav-link" href="{{ url('all-orders') }}" target="">
-                              <span class="menu-title">Sales (s)</span>
+                              <span class="menu-title">Online Sales (s)</span>
                               <i class="mdi  mdi-24px  menu-icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                           viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
